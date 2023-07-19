bf2b991d5d1ac758af4250899db05daed12922cf
unknown <throwaway0x069420@gmail.com>
2023-07-19 00:19:07 -0400

    added implementation for new, faster and simpler pipeline

---
+++ helpers.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+# https://stackoverflow.com/questions/32609098/how-to-fast-change-image-brightness-with-python-opencv
+def increase_brightness(img, value=30):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    lim = 255 - value
+    v[v > lim] = 255
+    v[v <= lim] += value
+
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 print("Starting up...")
 import cv2
 import numpy as np
+import color_track_return_rectangle as ct2r
+import helpers
 
 device = 0
 latch = True
-color_bottom = 0
-color_top = 0
-camera_input = 0
-camera_input_hsv = 0
+detections = []
 
 if device == None: device = int(input("--Which device (enter an int)? "))
 
@@ -18,10 +17,7 @@ if not cap.isOpened():
 
 
 def onclick(event, x, y, *argv):
-    global camera_input_hsv, color_bottom, color_top
-    print("Selected pixel: "+ str(camera_input_hsv[y,x]) )
-    color_bottom = camera_input_hsv[y,x][0] - 5
-    color_top    = camera_input_hsv[y,x][0] + 5
+    pass
     
     
 cv2.namedWindow("input")
@@ -34,15 +30,18 @@ while latch:
     timer = cv2.getTickCount()
     ret, camera_input = cap.read()
     if (ret):
+        camera_input = helpers.increase_brightness(camera_input, value=10)
         cv2.imshow("input", camera_input)
         
-        # apply a color mask
-        camera_input_hsv = cv2.cvtColor(camera_input, cv2.COLOR_BGR2HSV)
-        lower_bound = np.array([color_bottom,50,50])
-        upper_bound = np.array([color_top,255,255])
-        mask = cv2.inRange(camera_input_hsv, lower_bound, upper_bound)
-        camera_input = cv2.bitwise_and(camera_input,camera_input, mask=mask)
-
+        # get polygons back out of the tracking methods
+        camera_input = ct2r._attempt_detection(camera_input, {"colormasks":
+        [
+            {"colormask_upper": ct2r.colors["upper_dark_blue"], "colormask_lower": ct2r.colors["lower_dark_blue"]},
+            {"colormask_upper": ct2r.colors["upper_light_blue"], "colormask_lower": ct2r.colors["lower_light_blue"]},
+            {"colormask_upper": ct2r.colors["upper_cyan"], "colormask_lower": ct2r.colors["lower_cyan"]},
+            
+        ]
+        })
         
         cv2.imshow("output", camera_input)
         
